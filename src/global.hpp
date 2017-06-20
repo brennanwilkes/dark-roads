3d00f7a213258203399dcb65bc559a5bf69a2276
Brennan Wilkes <brennan@codexwilkes.com>
2017-06-19 19:35:38 -0700

    A better hello world!

---
+++ src/global.hpp
@@ -0,0 +1,6 @@
+#ifndef GLOBAL_H
+#define GLOBAL_H
+
+extern int scene;	//Just an example on how to do a global
+
+#endif
--- src/main.cc
+++ src/main.cc
@@ -1,3 +1,14 @@
+/*
+
+Summer project for IDS Best Team but probally just Brennan unless other people are interested
+
+SFML 2.4.2
+
+DARK ROADS
+
+*/
+
+//C++ Includes
 #include <iostream>
 #include <map>
 #include <string>
@@ -6,12 +17,40 @@
 #include <fstream>
 #include <cstdlib>
 
+//SFML Includes
+#include <SFML/Graphics.hpp>
+
+//User Includes
+#include "player.hpp"
+#include "gameObject.hpp"
+#include "global.hpp"
+
+
 using namespace std;
+using namespace sf;
+
+
+int scene;	//Just an example on how to do a global
 
 int main(int argc, char *argv[]) {
 	
-	cout<<"Hello World"<<endl;
-	
-	
+	RenderWindow window(VideoMode(800, 800), "Hello World!");
+    CircleShape shape(200.f);
+    shape.setFillColor(Color::Green);
+
+    while (window.isOpen())
+    {
+        Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
 	return 0;
 }
